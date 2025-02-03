99a1ad0905b2e35c9ff45960224ffa54fa81713d
gravitino <ngobinh0312@gmail.com>
2025-02-03 15:55:23 +0700

    add charitan-api-gateway

---
+++ scripts/build.sh
@@ -0,0 +1,5 @@
+# docker buildx build -t charitan-go/charitan-api-gateway charitan-api-gateway
+
+docker buildx build -t charitan-go/auth-server auth-server
+
+# docker buildx build -t charitan-go/profile-server profile-server
---
+++ scripts/up.sh
@@ -0,0 +1,1 @@
+docker compose -f docker-compose/compose.yml up -d --force-recreate
