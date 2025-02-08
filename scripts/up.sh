d93d6068e9973d95ebd1ae5e527fab2f8f4d4788
gravitino <ngobinh0312@gmail.com>
2025-02-08 22:24:53 +0700

    readd new client

--- scripts/up.sh
+++ scripts/up.sh
@@ -1,3 +1,3 @@
 docker compose -f docker-compose/compose.yml down
 
-docker compose -f docker-compose/compose.yml up -d --force-recreate --build
+docker compose -f docker-compose/compose.yml up --force-recreate --build
