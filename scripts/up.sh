c6543acd820d53715f49601c5107e713307c4f00
gravitino <ngobinh0312@gmail.com>
2025-02-15 09:00:13 +0700

    finish setup consumer rabbitmq

--- scripts/up.sh
+++ scripts/up.sh
@@ -1,3 +1,4 @@
 docker compose -f docker-compose/compose.yml down
-
 docker compose -f docker-compose/compose.yml up -d --force-recreate --build
+
+# docker compose -f docker-compose/compose.yml up -d --build key-server
