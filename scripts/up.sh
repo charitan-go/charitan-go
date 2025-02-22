d937170cf252aa96a0b2ecf65f1d52719a9f7bb1
gravitino <ngobinh0312@gmail.com>
2025-02-22 11:03:49 +0700

    finish send email to donor after registration

--- scripts/up.sh
+++ scripts/up.sh
@@ -1,4 +1,5 @@
 docker compose -f docker-compose/compose.yml down
+
 docker compose -f docker-compose/compose.yml up -d --force-recreate --build
 
 # docker compose -f docker-compose/compose.yml up -d --build key-server
